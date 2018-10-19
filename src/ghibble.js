9c92326b718b172b7f84f91ac92fa1bde111856f
Ahmed Khokar <amkhokar@gmail.com>
2018-10-19 10:43:32 -0700

    add release_date

--- src/ghibble.js
+++ src/ghibble.js
@@ -14,3 +14,19 @@ export function getGhibble(title) {
       request.send();
     });
   }
+
+export function getPeople(peopleSrc) {
+  return new Promise(function (resolve, reject) {
+    let request = new XMLHttpRequest();
+    let url = peopleSrc;
+    request.onload = function () {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(Error(request.statusText));
+      }
+    };
+    request.open("GET", url, true);
+    request.send();
+  });
+}
--- src/main.js
+++ src/main.js
@@ -10,19 +10,22 @@ $(document).ready(function () {
     $('#result').text("");
     let filmName = $('#name').val();
     let promise = getGhibble(filmName);
-
     promise.then(function (response) {
       let body = JSON.parse(response);
-      for (let i = 0; i < body.length; i++) {
-        for (let prop in body[i]) {
-          if (prop === 'title' || prop === 'description') {
-            $('#result').append("<b>" + prop.toUpperCase() + "</b> : " + body[i][prop] + "<br>");
-          }
-        }
+      console.log(body);
+      
+      for (let prop in body[0]) {
+        if (prop === 'title' || prop === 'director' || prop == 'description' || prop === 'release_date' || prop === 'producer') {
+          $('#result').append(`<b> ${prop.toUpperCase()} :</b> ${body[0][prop]}` + "<br>");
+        }   
       }
+
+      
     }, function (error) {
       $('.showErrors').text(`There was an error processing your request : ${error.message}`);
     });
+
+    
 
 
   });
